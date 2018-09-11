7b0ae950c47d76e331f4173d7e4319542840d1fc
Draexx <p.carranza.is@gmail.com>
2018-09-11 13:21:32 -0400

    Create variablesdef.sh

---
+++ shellscript/scripts/variablesdef.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MiNombre=Juanito
+MiOS=Manjaro
+MiNo=7
+echo "Mi nombre es $MiNombre"
+echo "Estoy usando $MiOS"
+echo "Mi número es Mino"
+echo "favor corregir los errores de este script"
